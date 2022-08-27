625ee825933be1c0abb920e86f9531878b4d161a
zarifjan-baltayev <zrfcnb@gmail.com>
2022-08-27 14:47:23 +0300

    version-1.1

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "educationisfuture.best@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
